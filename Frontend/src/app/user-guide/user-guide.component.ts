deb35d62981991589d95667c23d1d31d3155ebce
enriccoverindra <enriccoverindra@gmail.com>
2024-07-27 15:46:22 +0700

    add user guide page

--- Frontend/src/app/user-guide/user-guide.component.ts
+++ Frontend/src/app/user-guide/user-guide.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from '../navbar/navbar.component';
 @Component({
   selector: 'app-user-guide',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './user-guide.component.html',
   styleUrl: './user-guide.component.css'
 })
