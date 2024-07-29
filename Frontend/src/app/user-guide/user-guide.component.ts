e53c2492452b4ef4e267c929bcc9150cc4e12f2b
enriccoverindra <enriccoverindra@gmail.com>
2024-07-29 16:13:13 +0700

    add user guide account

--- Frontend/src/app/user-guide/user-guide.component.ts
+++ Frontend/src/app/user-guide/user-guide.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user-guide',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, CommonModule],
   templateUrl: './user-guide.component.html',
   styleUrl: './user-guide.component.css'
 })
 export class UserGuideComponent {
-
+selectedTab: any;
+onTabSelect(arg0: string) {
+throw new Error('Method not implemented.');
+}
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+}
 }
